5cead1c71e72c0dab987b9980db521d81b635042
manuh <manuh@LAPTOP-EU21NM8B>
2023-01-20 11:08:52 -0500

    my first commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author manuh
+ *
+ */
+module webhook {
+}
---
+++ src/webhook/srihi.java
@@ -0,0 +1,10 @@
+package webhook;
+
+public class srihi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello,srihi");
+	}
+
+}
